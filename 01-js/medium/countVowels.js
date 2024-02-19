aa591873e3f2f5880357255004d70e98ffe61ed1
Prathik Anand Krishnan <prathik.anandkrishnan@arup.com>
2024-02-19 23:10:49 +0530

    wk-01-medium-countVowels assignment

--- 01-js/medium/countVowels.js
+++ 01-js/medium/countVowels.js
@@ -6,7 +6,14 @@
 */
 
 function countVowels(str) {
-    // Your code here
+  let lower_str = str.toLowerCase();
+  let number_of_vowels = 0;
+
+  for (let index = 0; index < lower_str.length; index++) {
+    if (lower_str[index] == "a" || lower_str[index] == "e" || lower_str[index] == "i" || lower_str[index] == "o" || lower_str[index] == "u")
+      number_of_vowels += 1;
+  }
+  return number_of_vowels;
 }
 
 module.exports = countVowels;
